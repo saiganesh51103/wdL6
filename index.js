c32909ab5f7534d67eaa0d0ec718a0c04b61285d
saiganesh51103 <108187180+saiganesh51103@users.noreply.github.com>
2023-01-06 15:04:33 +0530

    update index.js level 9

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const app = require("./app.js");
-const express = require("express");
-const path = require("path");
+const app = require("./app");
 
-app.set("view engine", "ejs");
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(process.env.PORT || 3000, () => {
+app.listen(3000, () => {
   console.log("Started express server at port 3000");
 });
